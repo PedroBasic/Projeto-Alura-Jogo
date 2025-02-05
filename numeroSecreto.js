9f8632d4de71b36f003b75cb92d9245e01fb33c4
PedroBasic <pedrosouzacraft12@gmail.com>
2025-02-04 21:11:49 -0300

    atualizado 04/02/2025

--- numeroSecreto.js
+++ numeroSecreto.js
@@ -2,24 +2,29 @@
 
 alert("Jogo do número secreto");
 
-let numeroSecreto = 5;
+let novoNumSecreto = parseInt(prompt("Digite o limite de número Secreto? "));
+let numeroSecreto = parseInt(Math.random() * novoNumSecreto) + 1;
 let escolhaNumero;
 let tentativas = 1;
 
 // while (enquanto)
 while (escolhaNumero != numeroSecreto){
-    escolhaNumero = prompt('Escolha um número de 1 a 10');
+    escolhaNumero = prompt(`Escolha um número de 1 a ${novoNumSecreto}`);
     
     // if (se) else (senão)
     if (escolhaNumero == numeroSecreto){
-        alert(`Você acertou o número secreto era: ${numeroSecreto}.`);
-        alert(`O Jogador chutou ${tentativas} vezes até acertar`);
+       break;
     }else{
         if (escolhaNumero > numeroSecreto){
             alert(`O número secreto e menor que ${escolhaNumero}`);
         }else{
             alert(`O número secreto e maior que ${escolhaNumero}`);
         }
+
+        // Abaixo utilizamos o '++'  para contabilizar toda vez que for tentado um número.
         tentativas++
     }
 }
+
+let palavraSecreta = tentativas > 1 ? 'Tentativa.' : 'tentativas.';
+alert(`Você acertou o número secreto era: ${numeroSecreto}. Com ${tentativas} ${palavraSecreta}`);
